2b161ff3d8b8d8b2d460bcd03e51ec1a8e8d7557
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-03 01:11:18 +0700

    chore: :art: format space, import statements

--- src/app/(authorized)/layout.tsx
+++ src/app/(authorized)/layout.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
-import { RootPath } from '@constants/enum';
 import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
 import { LoadingPageMnt } from '@components/Common/Display/loading';
+import { RootPath } from '@constants/enum';
 
 /**
+ * Renders the authorized layout component.
  * @description Require user to be logged in to access the page
+ *
+ * @param {Readonly<{ children: React.ReactNode }>} children - The children elements to be rendered within the layout.
+ * @return {JSX.Element} The rendered component.
  */
-export default function AuthorizedLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function AuthorizedLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>): JSX.Element {
     const { data: session } = useSession();
 
     const router = useRouter();
