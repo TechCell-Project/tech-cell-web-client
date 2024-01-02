5edbacfe3a5f556f9fcd401c6c7c47d8be3d8099
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-03 01:09:49 +0700

    refactor(app): :recycle: move `login` page to `unauth` group

--- src/app/(unauthorized)/dang-nhap/layout.tsx
+++ src/app/(unauthorized)/dang-nhap/layout.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { redirect } from 'next/navigation';
 import { Metadata } from 'next/types';
-import { auth } from '@libs/next-auth';
-import { RootPath } from '@constants/enum';
 
 export const metadata: Metadata = {
     title: 'Đăng Nhập - TechCell - Điện thoại, phụ kiện chính hãng',
 };
 
 export default async function LoginLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    const session = await auth();
-
-    if (session?.user) {
-        redirect(RootPath.Home);
-    }
-
     return <>{children}</>;
 }
