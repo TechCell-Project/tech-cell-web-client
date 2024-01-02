cf4cb4839e3a108443b7b790431ecce767c3fbde
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-03 00:20:02 +0700

    feat(app): :sparkles: update protected route

--- src/app/(authorized)/layout.tsx
+++ src/app/(authorized)/layout.tsx
@@ -1,17 +1,23 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
 import { RootPath } from '@constants/enum';
-import { auth } from '@libs/next-auth';
+import { useSession } from 'next-auth/react';
+import { LoadingPageMnt } from '@components/Common/Display/loading';
 
 /**
  * @description Require user to be logged in to access the page
  */
-export default async function AuthorizedLayout({
-    children,
-}: Readonly<{ children: React.ReactNode }>) {
-    const session = await auth();
+export default function AuthorizedLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    const { data: session } = useSession();
+
+    const router = useRouter();
+    const pathname = usePathname();
 
     if (!session?.user) {
-        return redirect(RootPath.Login);
+        const loginUrlCallback = `${RootPath.Login}?callbackUrl=${pathname ?? '/'}`;
+        router.push(loginUrlCallback);
+        return <LoadingPageMnt isLoading />;
     }
 
     return <>{children}</>;
