607e69974717241662da95bf18cf587333ceaa1d
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-02 19:55:45 +0700

    refactor(socket): :recycle: refactor socket to libs with two step effect

--- src/components/Provider/SocketProvider.tsx
+++ src/components/Provider/SocketProvider.tsx
@@ -2,38 +2,60 @@
 
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@store/store';
-import socketConfig from '@config/socket_io.config';
 import { NotificationModel } from '@models/Notification';
 import { setPushNotifySocket, setSocket } from '@store/slices/notificationSlice';
 import { useSession } from 'next-auth/react';
+import { SubEvent } from '@libs/socket-io';
+import { getSocket } from '@libs/socket-io/socket-io';
 
-export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+export function SocketProvider({ children }: Readonly<{ children: React.ReactNode }>) {
     const dispatch = useAppDispatch();
     const { data: session } = useSession();
     const { socket } = useAppSelector((state) => state.notifications);
 
+    const handleNewOrderAdmin = (data: { time: string; notifications: NotificationModel }) => {
+        console.log(data);
+        dispatch(setPushNotifySocket(data.notifications));
+    };
+
+    const handleAllUserRoom = (data: { time: string; notifications: NotificationModel }) => {
+        console.log(data);
+        dispatch(setPushNotifySocket(data.notifications));
+    };
+
+    const handleUserIdRoom = (data: { time: string; notifications: NotificationModel }) => {
+        console.log(data);
+        dispatch(setPushNotifySocket(data.notifications));
+    };
+
+    const handleRoleRoom = (data: { time: string; notifications: NotificationModel }) => {
+        console.log(data);
+        dispatch(setPushNotifySocket(data.notifications));
+    };
+
+    // This effect runs only when the user logs in or out
     useEffect(() => {
-        if (session) {
-            socketConfig.on('connect', () => {
-                console.log('Connected To socket Server! 🙃🙃🙃');
-            });
+        if (session?.user && !socket?.connected) {
+            const socketInstance = getSocket(session?.user?.accessToken);
 
-            socketConfig.on('new-order-admin', (data: { time: string; notifications: NotificationModel }) => {
-                console.log(data);
-                dispatch(setPushNotifySocket(data.notifications));
-            });
+            socketInstance.on(SubEvent.newOrderAdmin, handleNewOrderAdmin);
+            socketInstance.on(SubEvent.allUserRoom, handleAllUserRoom);
+            socketInstance.on(SubEvent.userIdRoom(session?.user?._id), handleUserIdRoom);
+            socketInstance.on(SubEvent.roleRoom(session?.user?.role), handleRoleRoom);
 
-            dispatch(setSocket(socketConfig));
-        } else {
-            if (socket && socket.connected) {
-                console.log('Disconnected socket Server! 🙃');
-                socket.disconnect();
-                dispatch(setSocket(null));
-            }
+            dispatch(setSocket(socketInstance));
         }
-    }, [session]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [session?.user, dispatch]);
 
-    return (
-        <>{children}</>
-    );
-};
+    // This effect runs only when the socket connection status changes
+    useEffect(() => {
+        if (!session?.user && socket?.connected) {
+            dispatch(setSocket(null));
+            socket.disconnect();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [session?.user, socket?.connected]);
+
+    return <>{children}</>;
+}
