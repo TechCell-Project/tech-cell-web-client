15cfd58d7cf3b9c11f78e1175ed27631f67293b7
lehuygiang28 <lehuygiang28@gmail.com>
2024-01-08 19:37:02 +0700

    feat(app): :recycle: scans directory to get a data for static public contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { HomePage } from '@components/Common';
 import Typography from '@mui/material/Typography';
-import { CarouselComponent } from '@components/Form';
+import { CarouselComponent, CarouselComponentProps } from '@components/Form';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import { benefitSection } from '@constants/static.const';
+import { HOME_SLOGAN, BENEFIT_SECTION } from '@constants/contents/common.constant';
+import { getPublicContents } from '@utils/directory.util';
+
+function getStaticContents(): CarouselComponentProps {
+    const carouselImages = getPublicContents('carousel_img');
+    return {
+        carouselImages,
+    };
+}
 
 export default function Home() {
+    const { carouselImages } = getStaticContents();
+
     return (
         <>
             <Typography
@@ -21,31 +31,31 @@ export default function Home() {
                     display: { xs: 'none', sm: 'none', md: 'block' },
                 }}
             >
-                UPGRADE YOUR CONNECTIVITY: SMART DEALS, SMARTER PHONES!
+                {HOME_SLOGAN}
             </Typography>
 
-            <CarouselComponent />
+            <CarouselComponent carouselImages={carouselImages} />
 
             <HomePage />
 
             <Box sx={{ bgcolor: '#fafafa', p: '40px 0' }}>
                 <Container sx={{ maxWidth: '1320px !important' }}>
                     <Grid container spacing={2} sx={{ alignItems: 'center' }}>
-                        {benefitSection.map((benefit) => (
-                            <Grid item xs={6} md={3} key={benefit.title}>
+                        {BENEFIT_SECTION.map(({ Icon, title, desc }) => (
+                            <Grid item xs={6} md={3} key={title}>
                                 <Stack
                                     direction='row'
                                     gap={3}
                                     alignItems='center'
                                     justifyContent={{ md: 'center', xs: 'flex-start' }}
                                 >
-                                    <benefit.icon sx={{ height: '32px', width: 'auto' }} />
+                                    <Icon sx={{ height: '32px', width: 'auto' }} />
                                     <Stack direction='column'>
                                         <Typography fontSize='16px' fontWeight={600} mb='4px'>
-                                            {benefit.title}
+                                            {title}
                                         </Typography>
                                         <Typography fontSize='13px' sx={{ opacity: 0.9 }}>
-                                            {benefit.desc}
+                                            {desc}
                                         </Typography>
                                     </Stack>
                                 </Stack>
