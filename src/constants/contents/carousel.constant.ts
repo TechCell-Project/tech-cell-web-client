8cc74d64a60249b0bda78705656266de7e78e77f
sondoannam <sondoannam202@gmail.com>
2024-01-09 14:43:44 +0700

    refactor(import): :art: fix cannot get images src sometimes

--- src/constants/contents/carousel.constant.ts
+++ src/constants/contents/carousel.constant.ts
@@ -1,8 +1,12 @@
 import { CarouselComponentProps } from '@components/Form/CarouselComponent';
+import Img1 from '@public/carousel_img/img1.png';
+import Img2 from '@public/carousel_img/img2.png';
+import Img3 from '@public/carousel_img/img3.png';
+import Img4 from '@public/carousel_img/img4.png';
 
 export const IMAGE_CAROUSEL: CarouselComponentProps['carouselImages'] = [
-    '/carousel_img/img1.png',
-    '/carousel_img/img2.png',
-    '/carousel_img/img3.png',
-    '/carousel_img/img4.png',
+    Img1.src,
+    Img2.src,
+    Img3.src,
+    Img4.src,
 ];
