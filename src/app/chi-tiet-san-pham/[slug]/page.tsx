4317875dd0a792f2176bc4c2ce9c51c9cc524932
sondoannam <sondoannam202@gmail.com>
2024-03-09 15:10:48 +0700

    fix(components): :sparkles: update not found page on product detail page

--- src/app/chi-tiet-san-pham/[slug]/page.tsx
+++ src/app/chi-tiet-san-pham/[slug]/page.tsx
@@ -9,19 +9,24 @@ import { useAppDispatch, useAppSelector } from '@/store/store';
 import { extractIdFromSlug } from '@utils';
 import { getDetailsProduct } from '@/store/slices/productSlice';
 import { ProductDetail } from '@/components/Common/Product/ProductDetail';
+import NotFound from './NotFound';
 
 export default function Page({ params }: Readonly<{ params: { slug: string } }>) {
     const dispatch = useAppDispatch();
     const idExtractedFromSlug = extractIdFromSlug(params.slug);
 
-    const { product } = useAppSelector((state) => state.product);
+    const { product, isLoading } = useAppSelector((state) => state.product);
 
     if (!product || product._id !== idExtractedFromSlug) {
         dispatch(getDetailsProduct(idExtractedFromSlug));
     }
 
-    if (!product) {
+    if (isLoading) {
         return <LoadingPage />;
+    }
+
+    if (!product) {
+        return <NotFound />;
     }
 
     return <ProductDetail product={product} />;
